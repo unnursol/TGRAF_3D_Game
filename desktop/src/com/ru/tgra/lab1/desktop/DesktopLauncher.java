4b8d927888c70918d72231742253421f0706179c
Birkir Brynjarsson <birkir@birkir.is>
2017-11-01 17:29:09 +0000

    Delete everything, start with files from Kari

--- desktop/src/com/ru/tgra/lab1/desktop/DesktopLauncher.java
+++ desktop/src/com/ru/tgra/lab1/desktop/DesktopLauncher.java
@@ -1,19 +1,23 @@
 package com.ru.tgra.lab1.desktop;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics.DisplayMode;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.ru.tgra.Game;
+import com.ru.tgra.game.LabMeshTexGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 
-		config.title = "aMazeBalls"; // or whatever you like
-		config.width = 1200;  //experiment with
-		config.height = 800;  //the window size
-		config.x = 150;
-		config.y = 50;
+		config.title = "Mesh and Texture exercise"; // or whatever you like
+		config.width = 1024;  //experiment with
+		config.height = 512;  //the window size
+		config.x = 80;
+		config.y = 80;
+		//config.fullscreen = true;
+		
 
-		new LwjglApplication(new Game(), config);
+		new LwjglApplication(new LabMeshTexGame(), config);
 	}
 }
